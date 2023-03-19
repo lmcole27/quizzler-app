8f3c5bd7e75b7aed32fadee6d4db9e7850102276
Linda Cole <lindacolenl@gmail.com>
2023-03-18 21:44:18 -0400

    Updated User Interface

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,6 +8,7 @@ class QuizBrain:
         self.score = 0
         self.question_list = q_list
         self.current_question = None
+        self.is_right = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -16,16 +17,13 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         q_text = html.unescape(self.current_question.text)
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
+        if user_answer == correct_answer:
             self.score += 1
-            print("You got it right!")
+            self.is_right = True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            self.is_right = False
+        return self.is_right, self.score, self.question_number
--- ui.py
+++ ui.py
@@ -1,2 +1,67 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+
+class UserInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(height=1200, width=1000, pady=50, padx=50, bg=THEME_COLOR)
+        self.score = Label(text="Score: 0", background=THEME_COLOR, font=("Arial", 16, "normal"), foreground="white")
+        self.score.grid(row=0, column=1, columnspan=1)
+        self.canvas = Canvas(height=250, width=300, highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150, 125,
+            width=280, justify="center",
+            text="question text goes here",
+            font=("Arial", 20, "italic"), fill=THEME_COLOR,
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        self.incorrect_image = PhotoImage(file="images/false.png")
+        self.incorrect_button = Button(
+            image=self.incorrect_image,
+            highlightthickness=0, padx=100, pady=100,
+            command=self.user_false)
+        self.incorrect_button.grid(row=2, column=1)
+        self.correct_image = PhotoImage(file="images/true.png")
+        self.correct_button = Button(
+            image=self.correct_image,
+            highlightthickness=0, padx=100, pady=100,
+            command=self.user_true
+        )
+        self.correct_button.grid(row=2, column=0)
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="Thanks for playing!")
+            self.incorrect_button.config(state="disabled")
+            self.correct_button.config(state="disabled")
+
+    def user_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def user_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, response):
+        is_right = response[0]
+        score = response[1]
+        asked_questions = response[2]
+        if is_right:
+            self.canvas.config(bg="green")
+            self.score.config(text=f"Score: {score} / {asked_questions}")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, lambda: self.get_next_question())
